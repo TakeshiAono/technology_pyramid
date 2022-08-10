474283abfd85ddf07a05a75b9f368e6fb2ecafd7
aono <takeshi.aono001@gmail.com>
2022-08-10 13:20:14 +0900

    header navのコンポ―ネット生成

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -62,7 +62,6 @@ class MyPagesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_q
-      byebug
       @q = Work.ransack(params[:q])
     end
 
