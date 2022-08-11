cc3a0eef087edf40ad165532ded906698e873295
aono <takeshi.aono001@gmail.com>
2022-08-11 23:20:16 +0900

    linksとlink_access_counterのテーブルにseedデータ生成、および全seedデータ実装完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,3 +67,66 @@ k = 1
   )
   k += 1
 end
+
+Link.create!(
+  title: "クラスについて（qiita記事）",
+  url: "https://qiita.com/shizen-shin/items/09cf07f9b9f5c4977a14",
+  good_counter: 100,
+  technology_id: 1
+)
+
+Link.create!(
+  title: "クラスの基礎知識まとめ（qiita記事）",
+  url: "https://qiita.com/naotospace/items/654b285a7bcbd82b1cff",
+  good_counter: 20,
+  technology_id: 1
+)
+
+Link.create!(
+  title: "クラスの概念整理してみた（qiita記事）",
+  url: "https://qiita.com/kohei_04/items/df462ac6578c0fb670bc",
+  good_counter: 50,
+  technology_id: 1
+)
+
+Link.create!(
+  title: "クラス毎のよく使う操作まとめ（qiita記事）",
+  url: "https://qiita.com/ren0826jam/items/24d28839d307ffa825fc",
+  good_counter: 80,
+  technology_id: 1
+)
+
+Link.create!(
+  title: "クラスとインスタンスの違い（qiita記事）",
+  url: "https://qiita.com/yamaday0u/items/dca132e46b390c9af51b",
+  good_counter: 3,
+  technology_id: 1
+)
+
+LinkAccessCounter.create!(
+  pyramid_id: 1,
+  link_id: 1,
+  counter: 2000
+)
+
+LinkAccessCounter.create!(
+  pyramid_id: 1,
+  link_id: 2,
+  counter: 300
+)
+
+LinkAccessCounter.create!(
+  pyramid_id: 1,
+  link_id: 3,
+  counter: 500
+)
+LinkAccessCounter.create!(
+  pyramid_id: 1,
+  link_id: 4,
+  counter: 80
+)
+LinkAccessCounter.create!(
+  pyramid_id: 1,
+  link_id: 5,
+  counter: 300
+)
