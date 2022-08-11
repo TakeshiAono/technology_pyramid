7d2a3280b0a6bb9cc031e70e6472782fa677cc7a
aono <takeshi.aono001@gmail.com>
2022-08-11 22:25:02 +0900

    pyramidモデルにアソシエーション追加、およびテーブルに外部キー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_115347) do
+ActiveRecord::Schema.define(version: 2022_08_11_130017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,10 +37,10 @@ ActiveRecord::Schema.define(version: 2022_08_11_115347) do
 
   create_table "pyramids", force: :cascade do |t|
     t.boolean "public_flag"
-    t.bigint "technology_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["technology_id"], name: "index_pyramids_on_technology_id"
+    t.bigint "parent_technology_id"
+    t.bigint "child_technology_id"
   end
 
   create_table "technologies", force: :cascade do |t|
@@ -93,7 +93,8 @@ ActiveRecord::Schema.define(version: 2022_08_11_115347) do
   add_foreign_key "link_access_counters", "links"
   add_foreign_key "link_access_counters", "pyramids"
   add_foreign_key "links", "technologies"
-  add_foreign_key "pyramids", "technologies"
+  add_foreign_key "pyramids", "technologies", column: "child_technology_id"
+  add_foreign_key "pyramids", "technologies", column: "parent_technology_id"
   add_foreign_key "technologies", "technologies", column: "lower_technology"
   add_foreign_key "technologies", "technologies", column: "upper_technology"
   add_foreign_key "technologies", "works"
