ee6daaaf973da30eea1551d55afd9cfe6cb423f7
aono <takeshi.aono001@gmail.com>
2022-08-13 13:02:08 +0900

    users/sessions#newをroot画面設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "my_pages#index"
+  # root "users/sessions#new"
   get 'searches/index'
   resources :links
   resources :pyramids
@@ -11,12 +11,16 @@ Rails.application.routes.draw do
   end
   resources :works
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, :controllers => {
+    # :sessions => 'users/sessions',
+    :registrations => 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
   devise_scope :user do
+    root "users/sessions#new"
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 end
