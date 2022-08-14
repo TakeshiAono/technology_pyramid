41c9a5957b5fee2bed685262841b1491a4132ff0
aono <takeshi.aono001@gmail.com>
2022-08-14 17:52:42 +0900

    rails d model link_access_counterを実施

--- app/models/link_access_counter.rb
+++
@@ -1,4 +0,0 @@
-class LinkAccessCounter < ApplicationRecord
-  belongs_to :pyramid
-  belongs_to :link
-end
--- test/models/link_access_counter_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LinkAccessCounterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
