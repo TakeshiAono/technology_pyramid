e1b6d2eec422c1431a30fbbaaf6fe5e3dba26e97
aono <takeshi.aono001@gmail.com>
2022-08-14 17:48:38 +0900

    rails d model pyramidsを実施

--- app/models/pyramid.rb
+++
@@ -1,6 +0,0 @@
-class Pyramid < ApplicationRecord
-  has_many :link_access_counters, dependent: :destroy
-  # belongs_to :technology, class_name: "Technology", foreign_key:"lower_technology", optional: true
-  belongs_to :parent_technology, class_name: "Technology", foreign_key: 'parent_technology_id'
-  belongs_to :child_technology, class_name: "Technology", foreign_key: 'child_technology_id'
-end
--- test/models/pyramid_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PyramidTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
