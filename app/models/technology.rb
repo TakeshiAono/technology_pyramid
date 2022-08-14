2b5ab23c674d84b690af46c47924af6c01d9250d
aono <takeshi.aono001@gmail.com>
2022-08-14 19:33:32 +0900

    Technology model のアソシエーション実装

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,6 +1,5 @@
 class Technology < ApplicationRecord
   has_many :links, dependent: :destroy
   belongs_to :work
-  belongs_to :upper_tech, class_name: "Technology", foreign_key:"upper_technology", optional: true
-  belongs_to :lower_tech, class_name: "Technology", foreign_key:"lower_technology", optional: true
+  has_many :hierarckies, dependent: :destroy
 end
