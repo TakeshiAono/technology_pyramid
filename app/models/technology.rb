b5f1f0b1604cf20734cce429de9c1b59e150b3c4
aono <takeshi.aono001@gmail.com>
2022-08-23 23:12:19 +0900

    app/models/technology.rb アソシエーション修正

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -4,7 +4,7 @@ class Technology < ApplicationRecord
   # has_many :lower_technologies, class_name: "hierarckies", foreign_key: :technology_id
   
   has_many :hierarckies, dependent: :destroy
-  has_many :lower_technologies, through: :hierarckies, source: :upper_technologyaaa
+  has_many :lower_technologies, through: :hierarckies, source: :upper_technology
 
   has_many :lower_hierarckies,class_name: "Hierarcky", foreign_key: :lower_technology_id
   has_many :upper_technologies, through: :lower_hierarckies, source: :technology
