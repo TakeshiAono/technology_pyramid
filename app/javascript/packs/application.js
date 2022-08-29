360cb0617a906ff4a56b380c28aa4b403d845954
aono <takeshi.aono001@gmail.com>
2022-08-29 13:59:29 +0900

    leader-lineの環境設定完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("leader-line")
+// require("leader-line").default
 
 
 
--- app/javascript/packs/pyramid.js
+++ app/javascript/packs/pyramid.js
@@ -1,4 +1,10 @@
-new LeaderLine(
-document.getElementById('start'),
-document.getElementById('end'));
+import { LeaderLine } from 'exports-loader?exports=LeaderLine!leader-line/leader-line.min.js';
 
+// 本題とは関係ないが、ページが pyramid.js を読み込む時点では
+// start と end の要素が存在しないので元のコードではエラーになる。
+// ページロード完了時に実行するように変える。
+document.addEventListener('DOMContentLoaded', () => {
+  new LeaderLine(
+  document.getElementById('start'),
+  document.getElementById('end'));
+});
