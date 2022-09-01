1e42d2ef0e03ca486433dd8515a38d28311494de
aono <takeshi.aono001@gmail.com>
2022-09-01 16:58:03 +0900

    配列の入れ子データ構造を実装

--- app/javascript/packs/pyramid.js
+++ app/javascript/packs/pyramid.js
@@ -4,38 +4,55 @@ import { LeaderLine } from 'exports-loader?exports=LeaderLine!leader-line/leader
 // start と end の要素が存在しないので元のコードではエラーになる。
 // ページロード完了時に実行するように変える。
 document.addEventListener('DOMContentLoaded', () => {
-  let technology_2 = Array.from(document.getElementsByClassName('technology-2'));
-  const array_technology_2 = [];
-  technology_2.forEach(node => array_technology_2.push(node.dataset.upperTechnologies.match(/\d+/g)));
-  console.log(array_technology_2);
+  // let technology_2 = Array.from(document.getElementsByClassName('technology-2'));
+  // const array_technology_2 = [];
+  // technology_2.forEach(node => array_technology_2.push(node.dataset.upperTechnologies.match(/\d+/g)));
+  // console.log(array_technology_2);
 
-  [0].dataset.upperTechnologies;
-  technology_2 = technology_2.match(/\d+/g);
+  let pyramid = Array(document.getElementById('top_technology').dataset.lowerTechnologies.match(/\d+/g));
+
+  let technology_array = new Array;
+  let technology_nodes = new Array;
+
+  technology_nodes = document.getElementById('hierarcky-2').children;
+  technology_nodes = Array.from(technology_nodes);
+  technology_nodes.forEach(technology_node => technology_array.push(technology_node.dataset.lowerTechnologies.match(/\d+/g)));
+  pyramid.push(technology_array)
+  technology_nodes = [];
+  technology_array = [];
+
+  technology_nodes = document.getElementById('hierarcky-3').children;
+  technology_nodes = Array.from(technology_nodes);
+  technology_nodes.forEach(technology_node => 
+    technology_array.push(technology_node.dataset.lowerTechnologies.match(/\d+/g)));
+  pyramid.push(technology_array)
+  technology_nodes = [];
+  technology_array = [];
+
+  technology_nodes = document.getElementById('hierarcky-4').children;
+  technology_nodes = Array.from(technology_nodes);
+  technology_nodes.forEach(technology_node => 
+    technology_array.push(technology_node.dataset.lowerTechnologies.match(/\d+/g)));
+  pyramid.push(technology_array)
+  technology_nodes = [];
+  technology_array = [];
+
+  technology_nodes = document.getElementById('hierarcky-5').children;
+  technology_nodes = Array.from(technology_nodes);
+  technology_nodes.forEach(technology_node => 
+    technology_array.push(technology_node.dataset.lowerTechnologies.match(/\d+/g)));
+  pyramid.push(technology_array)
+  technology_nodes = [];
+  technology_array = [];
+
+
+  console.log(pyramid);
+
+
+
 
 
   new LeaderLine(
-  document.getElementById('element-1'),
-  // document.getElementById('end');
-  document.getElementById('element-2'));
-
-  new LeaderLine(
-  document.getElementById('element-2'),
-  // document.getElementById('end');
-  document.getElementById('element-3'));
-
-  new LeaderLine(
-  document.getElementById('element-2'),
-  // document.getElementById('end');
-  document.getElementById('technology1'));
-
-  // new LeaderLine(
-  // document.getElementById('element-2'),
-  // document.getElementById('end');
-  // document.getElementById('technology2'));
-
-  // new LeaderLine(
-  // document.getElementById('element-1'),
-  // // document.getElementById('end');
-  // document.getElementById('element-3'));
-
-});
+  document.getElementById('top_technology'),
+  document.getElementById('element2-1'));
+  });
