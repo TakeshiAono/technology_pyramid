465eaa1ecb596c7e8447025fd7e1e94f5a113fe7
aono <takeshi.aono001@gmail.com>
2022-09-01 20:35:55 +0900

    leader-lineを各technology nodeに関連付け完了

--- app/javascript/packs/pyramid.js
+++ app/javascript/packs/pyramid.js
@@ -1,13 +1,6 @@
 import { LeaderLine } from 'exports-loader?exports=LeaderLine!leader-line/leader-line.min.js';
 
-// 本題とは関係ないが、ページが pyramid.js を読み込む時点では
-// start と end の要素が存在しないので元のコードではエラーになる。
-// ページロード完了時に実行するように変える。
 document.addEventListener('DOMContentLoaded', () => {
-  // let technology_2 = Array.from(document.getElementsByClassName('technology-2'));
-  // const array_technology_2 = [];
-  // technology_2.forEach(node => array_technology_2.push(node.dataset.upperTechnologies.match(/\d+/g)));
-  // console.log(array_technology_2);
 
   let technology_vector = {};
   let technology_nodes = new Array;
@@ -17,11 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
   technology_nodes = Array.from(technology_nodes);
   technology_nodes.forEach(technology_node => technology_vector[`${technology_node.id}`] = technology_node.dataset.lowerTechnologies.match(/\d+/g));
   pyramid.push(technology_vector)
-  // pyramid = Array(document.getElementById('top_technology').dataset.lowerTechnologies.match(/\d+/g));
-  // pyramid = Array(document.getElementById('top_technology').dataset.lowerTechnologies.match(/\d+/g));
   technology_vector = {};
   technology_nodes = [];
-
 
   technology_nodes = document.getElementById('hierarcky-2').children;
   technology_nodes = Array.from(technology_nodes);
@@ -34,43 +24,33 @@ document.addEventListener('DOMContentLoaded', () => {
   technology_nodes = Array.from(technology_nodes);
   technology_nodes.forEach(technology_node => technology_vector[`${technology_node.id}`] = technology_node.dataset.lowerTechnologies.match(/\d+/g));
   pyramid.push(technology_vector)
-  technology_nodes = [];
   technology_vector = {};
+  technology_nodes = [];
 
   technology_nodes = document.getElementById('hierarcky-4').children;
   technology_nodes = Array.from(technology_nodes);
   technology_nodes.forEach(technology_node => technology_vector[`${technology_node.id}`] = technology_node.dataset.lowerTechnologies.match(/\d+/g));
   pyramid.push(technology_vector)
-  technology_nodes = [];
   technology_vector = {};
+  technology_nodes = [];
 
   technology_nodes = document.getElementById('hierarcky-5').children;
   technology_nodes = Array.from(technology_nodes);
   technology_nodes.forEach(technology_node => technology_vector[`${technology_node.id}`] = technology_node.dataset.lowerTechnologies.match(/\d+/g));
   pyramid.push(technology_vector)
-  technology_nodes = [];
   technology_vector = {};
+  technology_nodes = [];
 
-
-  console.log(pyramid);
-
-  let ends
+  let ends;
+  let line;
   pyramid.forEach(hierarcky => Object.keys(hierarcky).forEach(function(start){
-      console.log(start)
-      ends = hierarcky[start]
-      console.log(ends)
-      ends.forEach(end =>
-        new LeaderLine(
-          document.getElementById(`${start}`),
-          document.getElementById(`${end}`)
-        )
+    ends = hierarcky[start]
+    ends.forEach(end =>
+      line = new LeaderLine(
+        document.getElementById(`${start}`),
+        document.getElementById(`${end}`),
+        {dash: {animation: true}, size: 4, startSocket: 'bottom', endSocket: 'top', path: 'straight' },
       )
-
-      // new LeaderLine(
-      // document.getElementById(`${start}`),
-      // document.getElementById(`${end}`))
+    )
   }));
-
-
-
 });
