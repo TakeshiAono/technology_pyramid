c164501bdda51a62d7fb3875b5fd79cab4d39929
aono <takeshi.aono001@gmail.com>
2022-09-03 17:09:08 +0900

    usersのspec仕様の構想計画完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    email {'guest@example.com'}
+    admin {true}
+    password {"aoao0101"}
+    password_confirmation {"aoao0101"}
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,44 +2,54 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   before do
+    FactoryBot.create(:user)
   end
 
-  # describe 'ユーザ新規登録機能' do
-  #   context '一般ユーザーがsign upした場合' do
-  #     example '作成したユーザーが表示される' do
-  #       visit new_user_registration_path
-  #       fill_in "user[name]", with: "test_user"
-  #       fill_in "user[email]", with: "testemail@gmail.com"
-  #       fill_in "user[password]", with: "testpassword"
-  #       fill_in "user[password_confirmation]", with: "testpassword"
-  #       click_on "commit"
-  #       expect(page).to have_content "test_user"
-  #       expect(page).to have_content "testemail@gmail.com"
-  #     end
-  #   end
-  # end
+  describe 'アカウント登録機能' do
+    context 'ユーザーがsign upした場合' do
+      example '正常にsign upできる' do
+        # visit new_user_registration_path
+        # fill_in "user[name]", with: "test_user"
+        # fill_in "user[email]", with: "testemail@gmail.com"
+        # fill_in "user[password]", with: "testpassword"
+        # fill_in "user[password_confirmation]", with: "testpassword"
+        # click_on "commit"
+        # expect(page).to have_content "test_user"
+        # expect(page).to have_content "testemail@gmail.com"
+      end
 
-  describe 'ユーザー機能' do
-    context 'ユーザーを新規作成した場合' do
-      it 'ユーザーの新規登録ができる' do
-        visit new_user_registration_path 
-        fill_in 'user[email]', with: 'test@test.com' 
-        fill_in 'user[password]', with: 'password' 
-        fill_in 'user[password_confirmation]', with: 'password' 
-        click_button 'アカウント登録' 
-        expect(page).to have_content 'アカウント登録が完了しました' 
-      end 
-    end 
-    context '登録した情報でログインした場合' do
-      it 'ログインができる' do
-        visit new_user_session_path 
-        fill_in 'user[email]', with: 'test1@test.com' 
-        fill_in 'user[password]', with: '12345678' 
-        click_button 'ログイン' 
-        expect(page).to have_content 'ログインしました' 
-      end 
+      example 'マイページに遷移できる' do
+      end
+    end
+
+    context 'emailが他のユーザーと重複した場合' do
+      example 'バリデーションエラーが出力される' do
+      end
+
+      example 'マイページ遷移しない' do
+      end
+
     end
   end
 
+  describe 'ログイン機能' do
+    context 'ゲストユーザーがログインした場合' do
+      example '正常にログインできる' do
+      end
+    end
 
+    context '一般ユーザーがログインした場合' do
+      example '正常にログインできる' do
+      end
+    end
+
+    context 'ユーザー以外がログインした場合' do
+      example 'pyramid画面とリンク画面へのアクセスを許可する' do
+      end
+      
+      example 'login画面に遷移させる' do
+      end
+    end
+
+  end
 end
