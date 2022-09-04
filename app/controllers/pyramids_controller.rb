4c6a00b4556363dda3d7e17eddd653e583568a1b
aono <takeshi.aono001@gmail.com>
2022-09-04 18:49:47 +0900

    インデント修正

--- app/controllers/pyramids_controller.rb
+++ app/controllers/pyramids_controller.rb
@@ -1,37 +1,37 @@
 class PyramidsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
-      session[:top_technology_id] = params[:format] if params[:format].present?
-      @top_technology = Technology.find(session[:top_technology_id])
+    session[:top_technology_id] = params[:format] if params[:format].present?
+    @top_technology = Technology.find(session[:top_technology_id])
 
-      @top_hierarckies = []
-      @first_hierarckies = []
-      @second_hierarckies = []
-      @third_hierarckies = []
-      @fourth_hierarckies = []
-      @hierarckies = []
+    @top_hierarckies = []
+    @first_hierarckies = []
+    @second_hierarckies = []
+    @third_hierarckies = []
+    @fourth_hierarckies = []
+    @hierarckies = []
 
-      @top_technology.lower_technologies.each do |technology|
-        @first_hierarckies << technology
+    @top_technology.lower_technologies.each do |technology|
+      @first_hierarckies << technology
+    end
+    
+    @first_hierarckies.each do |technology|
+      technology.lower_technologies.each do |technology2|
+        @second_hierarckies << technology2
       end
-      
-      @first_hierarckies.each do |technology|
-        technology.lower_technologies.each do |technology2|
-          @second_hierarckies << technology2
-        end
-      end
+    end
 
-      @second_hierarckies.each do |technology|
-        technology.lower_technologies.each do |technology2|
-          @third_hierarckies << technology2
-        end
+    @second_hierarckies.each do |technology|
+      technology.lower_technologies.each do |technology2|
+        @third_hierarckies << technology2
       end
+    end
 
-      @third_hierarckies.each do |technology|
-        technology.lower_technologies.each do |technology2|
-          @fourth_hierarckies << technology2
-        end
+    @third_hierarckies.each do |technology|
+      technology.lower_technologies.each do |technology2|
+        @fourth_hierarckies << technology2
       end
+    end
   end
 end
