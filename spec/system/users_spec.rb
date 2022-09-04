dae44ad49e4975cf4f44681cb38cff8ef08bfca1
aono <takeshi.aono001@gmail.com>
2022-09-04 21:50:03 +0900

    :recycle: users specをキーワード引数でDRYに改良

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,65 +3,49 @@ require 'rails_helper'
 RSpec.describe "Users", type: :system do
   describe 'アカウント登録機能' do
     context 'sign upした場合' do
+      def sign_up_form_input(name: "example", email: "guest@example.com", password: "example", password_confirmation: "example")
+        visit new_user_registration_path
+        fill_in "user[name]", with: name
+        fill_in "user[email]", with: email
+        fill_in "user[password]", with: password
+        fill_in "user[password_confirmation]", with: password_confirmation
+        click_on "commit"
+      end
+
       context '入力値が不適でsign upした場合' do
         example 'nameが空の場合、エラーメッセージが表示され、マイページに遷移しない' do
           visit new_user_registration_path
-          fill_in "user[name]", with: nil
-          fill_in "user[email]", with: "guest@example.com"
-          fill_in "user[password]", with: "example"
-          fill_in "user[password_confirmation]", with: "example"
-          click_on "commit"
+          sign_up_form_input(name: nil)
           expect(page).to have_content "入力してください"
           expect(current_path).not_to eq my_pages_path
         end
 
         example 'nameが10文字以上の場合、エラーメッセージが表示され、マイページに遷移しない' do
-          visit new_user_registration_path
-          fill_in "user[name]", with: "a"*11
-          fill_in "user[email]", with: "guest@example.com"
-          fill_in "user[password]", with: "example"
-          fill_in "user[password_confirmation]", with: "example"
-          expect(current_path).not_to eq my_pages_path
-          click_on "commit"
+          sign_up_form_input(name: "a"*11)
           expect(page).to have_content "文字以内で入力してください"
+          expect(current_path).not_to eq my_pages_path
         end
+
         example 'emailが空の場合、エラーメッセージが表示され、マイページに遷移しない' do
-          visit new_user_registration_path
-          fill_in "user[name]", with: "example"
-          fill_in "user[email]", with: nil
-          fill_in "user[password]", with: "example"
-          fill_in "user[password_confirmation]", with: "example"
-          click_on "commit"
+          sign_up_form_input(email: nil)
           expect(page).to have_content "入力してください"
           expect(current_path).not_to eq my_pages_path
         end
+
         example 'emailが30文字以上の場合、エラーメッセージが表示され、マイページに遷移しない' do
-          visit new_user_registration_path
-          fill_in "user[name]", with: "example"
-          fill_in "user[email]", with: "a"*19+"@example.com"
-          fill_in "user[password]", with: "example"
-          fill_in "user[password_confirmation]", with: "example"
-          click_on "commit"
+          sign_up_form_input(email: "a"*19+"@example.com")
           expect(page).to have_content "文字以内で入力してください"
           expect(current_path).not_to eq my_pages_path
         end
+
         example 'emailがフォーマットに従ってない場合、エラーメッセージが表示され、マイページに遷移しない' do
-          visit new_user_registration_path
-          fill_in "user[name]", with: "example"
-          fill_in "user[email]", with: "example"
-          fill_in "user[password]", with: "example"
-          fill_in "user[password_confirmation]", with: "example"
-          click_on "commit"
+          sign_up_form_input(email: "example")
           expect(current_path).to eq "/users/sign_up"
           expect(current_path).not_to eq my_pages_path
         end
+
         example 'passwordとpassword_confirmが一致しない場合、エラーメッセージが表示され、マイページに遷移しない' do
-          visit new_user_registration_path
-          fill_in "user[name]", with: "example"
-          fill_in "user[email]", with: "guest@example.com"
-          fill_in "user[password]", with: "example"
-          fill_in "user[password_confirmation]", with: "examplea"
-          click_on "commit"
+          sign_up_form_input(password: "example", password_confirmation: "examplea")
           expect(page).to have_content "パスワードの入力が一致しません"
           expect(current_path).not_to eq my_pages_path
         end
@@ -69,12 +53,7 @@ RSpec.describe "Users", type: :system do
 
       context '入力値が正常な時' do
         example 'マイページに遷移できる' do
-          visit new_user_registration_path
-          fill_in "user[name]", with: "example"
-          fill_in "user[email]", with: "guest@example.com"
-          fill_in "user[password]", with: "example"
-          fill_in "user[password_confirmation]", with: "example"
-          click_on "commit"
+          sign_up_form_input
           expect(current_path).to eq my_pages_path
         end
       end
