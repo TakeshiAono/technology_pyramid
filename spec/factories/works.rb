32c2e6008188e9ad892682cf7e9a9a0cf5c1c3cc
aono <takeshi.aono001@gmail.com>
2022-09-05 16:00:23 +0900

    #150 work model実装完了

--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :work do
-    
+    title {'test'}
+    public_flag {true}
+    active_flag {true}
+    association :user
   end
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -5,38 +5,38 @@ RSpec.describe Users, type: :model do
     let(:user){FactoryBot.create(:user)}
     context 'バリデーション失敗の場合' do
       example 'nameが空の場合、validationエラーになる' do
-        test_user = User.create(name: nil, email: 'guest@example.com', password: "example", password_confirmation: "example")
+        test_user = FactoryBot.build(:user, name: nil, email: 'guest@example.com', password: "example", password_confirmation: "example")
         expect(test_user).to be_invalid
       end
 
       example 'nameが10文字以上の場合、validationエラーになる' do
-        test_user = User.create(name: "a"*11, email: 'guest@example.com', password: "example", password_confirmation: "example")
+        test_user = FactoryBot.build(:user, name: "a"*11, email: 'guest@example.com', password: "example", password_confirmation: "example")
         expect(test_user).to be_invalid
       end
 
       example 'emailが空の場合、validationエラーになる' do
-        test_user = User.create(name: "example", email: nil, password: "example", password_confirmation: "example")
+        test_user = FactoryBot.build(:user, name: "example", email: nil, password: "example", password_confirmation: "example")
         expect(test_user).to be_invalid
       end
 
       example '他のemailと重複した場合、validationエラーになる' do
         user
-        test_user = User.create(name: "example", email: 'guest@example.com', password: "example", password_confirmation: "example")
+        test_user = FactoryBot.build(:user, name: "example", email: 'guest@example.com', password: "example", password_confirmation: "example")
         expect(test_user).to be_invalid
       end
 
       example 'emailがフォーマットに従ってない場合、validationエラーになる' do
-        test_user = User.create(name: "example", email: 'guestexample.com', password: "example", password_confirmation: "example")
+        test_user = FactoryBot.build(:user, name: "example", email: 'guestexample.com', password: "example", password_confirmation: "example")
         expect(test_user).to be_invalid
       end
 
       example 'emailが30文字以上の場合、validationエラーになる' do
-        test_user = User.create(name: "example", email: 'a'*19+'@example.com', password: "example", password_confirmation: "example")
+        test_user = FactoryBot.build(:user, name: "example", email: 'a'*19+'@example.com', password: "example", password_confirmation: "example")
         expect(test_user).to be_invalid
       end
       
       example 'passwordとpassword_confirmが一致しない場合、validationエラーになる' do
-        test_user = User.create(name: "example", email: 'guest@example.com', password: "example", password_confirmation: "examplea")
+        test_user = FactoryBot.build(:user, name: "example", email: 'guest@example.com', password: "example", password_confirmation: "examplea")
         expect(test_user).to be_invalid
       end
     end
