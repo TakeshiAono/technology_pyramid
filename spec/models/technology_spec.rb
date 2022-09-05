434070f63643d3e7066887f504da8f5e20d591ce
aono <takeshi.aono001@gmail.com>
2022-09-05 17:01:05 +0900

    #151 technology model spec実装完了

--- spec/models/technology_spec.rb
+++ spec/models/technology_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Technology, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション機能' do
+    context 'バリデーション失敗の場合' do
+      example 'nameが空の場合、validationエラーになる' do
+        technology = FactoryBot.build(:technology, name: nil)
+        expect(technology).to be_invalid
+      end
+
+      example 'titleが11文字以上場合、validationエラーになる' do
+        technology = FactoryBot.build(:technology, name: "a"*11)
+        expect(technology).to be_invalid
+      end
+    end
+
+    context 'バリデーション成功の場合' do
+      example 'technologissテーブルにレコード追加ができる' do
+        technology = FactoryBot.build(:technology)
+        expect(technology).to be_valid
+      end
+    end
+  end
 end
