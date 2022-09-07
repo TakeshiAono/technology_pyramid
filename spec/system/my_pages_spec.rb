e26a1612bcc3085dbc49fbfdd841353358eb9fb7
aono <takeshi.aono001@gmail.com>
2022-09-07 12:47:57 +0900

    #153 works system spec 実装完了

--- spec/system/my_pages_spec.rb
+++ spec/system/my_pages_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "MyPages", type: :system do
+  before do
+    FactoryBot.create(:work)
+    visit new_user_session_path
+    click_link 'ゲストログイン（閲覧用）'
+  end
   describe 'ワーク検索機能' do
     context '検索に該当するものがあった場合' do
-      before do
-        FactoryBot.create(:work)
-        visit new_user_session_path
-        click_link 'ゲストログイン（閲覧用）'
-      end
       example 'ワーク名を表示する' do
         visit my_pages_path
         fill_in 'q[title_cont]', with: 'test'
--- spec/system/works_spec.rb
+++ spec/system/works_spec.rb
@@ -1,9 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Works", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe 'CRUD機能' do
+    before do
+      FactoryBot.create(:work)
+      visit new_user_session_path
+      click_link 'ゲストログイン（閲覧用）'
+      visit works_path
+    end
+    
+    context '新規ワークを作成した場合' do
+      example '新しいワークレコードがワーク一覧ページで表示される' do
+        click_link '新規ワーク作成'
+        fill_in "work[title]", with: 'example'
+        click_on 'commit'
+        expect(page).to have_content "example"
+      end
+    end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    context 'ワークの詳細確認をした場合' do
+      example 'showページで表示される' do
+        click_link '詳細'
+        expect(page).to have_content "test"
+      end
+    end
+
+    context 'ワークを編集した場合' do
+      example '変更した内容がワーク一覧ページに反映される' do
+        click_link '編集'
+        fill_in "work[title]", with: 'change'
+        click_on 'commit'
+        expect(page).to have_content "change"
+      end
+    end
+
+    context 'ワークを削除した場合' do
+      example '削除したワークがワーク一覧ページから消えている' do
+        click_link '削除'
+        page.driver.browser.switch_to.alert.accept
+        expect(page).not_to have_content "test"
+      end
+    end
+  end
 end
