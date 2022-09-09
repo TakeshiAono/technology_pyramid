3fcd7cbe78165774cc9285b1f9fa4591def057fd
aono <takeshi.aono001@gmail.com>
2022-09-10 08:17:09 +0900

    :recycle: routes修正ver2

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,18 @@ Rails.application.routes.draw do
   resources :pyramids, only: %i[index]
   resources :technologies
   resources :my_pages, only: %i[index]
-  
   resources :works
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
   }
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+  
   devise_scope :user do
     root "users/sessions#new"
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
