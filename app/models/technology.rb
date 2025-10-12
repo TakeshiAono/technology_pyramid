815144a07a6c3a5883b387d02f8d70a53fd781bd
tka <takeshi.aono001@gmail.com>
2025-10-12 16:58:15 +0900

    テクノロジー親子情報を取得する機能を実装

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -13,4 +13,45 @@ class Technology < ApplicationRecord
     |attributes| attributes['lower_technology_id'].blank?
   },
   allow_destroy: true
+
+  def get_technologies_and_hierarckies
+    query = <<~SQL
+    -- 子テクノロジーのみを芋づる式に取得している
+      WITH
+        RECURSIVE pyramid_relations AS (
+          --　一番上の親はここでは取得せず本sqlの最後で追加している。
+          SELECT h.lower_technology_id AS child_tech_id, 1 AS layer
+          FROM hierarckies h
+          WHERE h.technology_id = ?
+          UNION ALL
+          -- 前段のレコードの子テクノロジーidを親とする子要素をunionする
+          SELECT h2.lower_technology_id AS child_tech_id, pr.layer + 1
+          FROM hierarckies h2
+          JOIN pyramid_relations pr ON h2.technology_id = pr.child_tech_id
+        ),
+        top_technology AS (SELECT * FROM technologies WHERE id = ?)
+
+      -- 親と子がセットになったレコードを取得
+      SELECT
+        t.*,
+        pr.layer,
+        pr.child_tech_id
+      FROM technologies t
+      JOIN pyramid_relations pr ON t.id = pr.child_tech_id
+      UNION ALL
+      -- トップの親と子がセットになったレコードを取得
+      SELECT
+        t.*,
+        0 AS layer,
+        h.lower_technology_id AS child_tech_id
+      FROM top_technology t
+      -- 子要素ができたと時に初めてhierarckiesレコードが生成されるため
+      -- 子要素が1つもないトップテクノロジーがあり得るため、inner join で消えないようleft outer joinを使用
+      LEFT OUTER JOIN hierarckies AS h
+      ON t.id = h.technology_id
+    SQL
+
+    sanitized_sql = ActiveRecord::Base.send(:sanitize_sql_array, [query, id, id])
+    ActiveRecord::Base.connection.execute(sanitized_sql).to_a
+  end
 end
