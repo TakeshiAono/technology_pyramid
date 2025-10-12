ca8f7d5dc64c5ef375aa7a3eaaf507e64ec82b92
tka <takeshi.aono001@gmail.com>
2025-10-13 00:27:30 +0900

    リファクタ

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -16,50 +16,48 @@ class Technology < ApplicationRecord
 
   def get_technologies_and_hierarckies
     query = <<~SQL
-    -- 子テクノロジーのみを芋づる式に取得している
       WITH
-        RECURSIVE pyramid_relations AS (
-          --　一番上の親はここでは取得せず本sqlの最後で追加している。
-          SELECT
-            h.technology_id AS upper_tech_id,
-            h.lower_technology_id AS current_tech_id,
-            1 AS layer
-          FROM hierarckies h
-          WHERE h.technology_id = ?
-          UNION ALL
-          -- 前段のレコードの子テクノロジーidを親とする子要素をunionする
-          SELECT
-            h2.technology_id AS upper_tech_id,
-            h2.lower_technology_id AS current_tech_id,
-            pr.layer + 1
-          FROM hierarckies h2
-          JOIN pyramid_relations pr ON h2.technology_id = pr.current_tech_id
-        ),
-        top_technology AS (SELECT * FROM technologies WHERE id = ?)
+        -- 子テクノロジーのみを芋づる式に取得している
+        RECURSIVE
+          pyramid_technologies AS (
+            SELECT
+              h.technology_id AS upper_tech_id,
+              h.lower_technology_id AS current_tech_id,
+              1 AS layer
+            FROM hierarckies h
+            WHERE h.technology_id = :top_technology_id -- 最上位のテクノロジーidが入力される
 
-      -- 親と子がセットになったレコードを取得
+            UNION ALL
+
+            -- 前段のレコードの子テクノロジーidを親とする子要素をレコードとして追加する
+            SELECT
+              h2.technology_id AS upper_tech_id,
+              h2.lower_technology_id AS current_tech_id,
+              pt.layer + 1
+            FROM hierarckies h2
+            JOIN pyramid_technologies pt ON h2.technology_id = pt.current_tech_id
+          )
+
+      -- 子と親の情報がセットになったレコードを取得
       SELECT
-        pr.upper_tech_id,
-        pr.current_tech_id,
+        pt.upper_tech_id,
+        pt.current_tech_id,
         t.name AS current_tech_name,
-        pr.layer AS current_layer
-      FROM technologies t
-      JOIN pyramid_relations pr ON t.id = pr.current_tech_id
+        pt.layer AS current_layer
+      FROM pyramid_technologies pt
+      JOIN technologies t ON pt.current_tech_id = t.id
       UNION ALL
-      -- トップの親と子がセットになったレコードを取得
+      -- 最上位用のレコードを追加
       SELECT
         NULL AS upper_tech_id,
         t.id AS current_tech_id,
         t.name AS current_tech_name,
         0 AS current_layer
-      FROM top_technology t
-      -- 子要素ができたと時に初めてhierarckiesレコードが生成されるため
-      -- 子要素が1つもないトップテクノロジーがあり得るため、inner join で消えないようleft outer joinを使用
-      LEFT OUTER JOIN hierarckies AS h
-      ON t.id = h.technology_id
+      FROM technologies t
+      WHERE id = :top_technology_id
     SQL
 
-    sanitized_sql = ActiveRecord::Base.send(:sanitize_sql_array, [query, id, id])
+    sanitized_sql = ActiveRecord::Base.send(:sanitize_sql_array, [query, {top_technology_id: id}])
     ActiveRecord::Base.connection.execute(sanitized_sql).to_a
   end
 end
