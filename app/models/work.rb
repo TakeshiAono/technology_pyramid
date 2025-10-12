cef68984f5298741da1c18e1f212ed9961dcb0d3
tka <takeshi.aono001@gmail.com>
2025-10-12 17:16:32 +0900

    リファクタ

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,6 @@
 class Work < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 10 }
   has_many :technologies, dependent: :destroy
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 10 }
 end
